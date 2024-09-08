7a2901a52ed7582a6a13eecc07af9bf39c3a2a8c
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-09-07 21:49:47 -0700

    final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,12 +4,12 @@ import { useSelector } from "react-redux";
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
 
-  const quantity = cart.reduce((s,cartItem) => {
+  const quantity = cart.reduce((s, cartItem) => {
     s = s + cartItem.quantity;
     console.log(s);
     return s;
-  },0);
-console.log(quantity);
+  }, 0);
+  console.log(quantity);
   return (
     <div className="cart">
       <h1>Your Cart({quantity})</h1>
@@ -19,7 +19,26 @@ console.log(quantity);
           <p>Your added items will appear here</p>
         </div>
       ) : (
-        <></>
+        <div className="cart__exist">
+          <article>
+            <section className="cart__exist__list">
+              {" "}
+              <div>
+                <h3>Classic Tiramisu</h3>
+                <p>
+                  <span className="number-saffron"> 1x</span>&nbsp;&nbsp;&nbsp;
+                  <span className="each-price">@$5.50</span>
+                  &nbsp;&nbsp;
+                  <span className="total-price">$5.50</span>
+                </p>
+              </div>
+              <div className="cart__exist__list--remove">
+                <img src="/assets/images/icon-remove-item.svg" alt="" />
+              </div>
+            </section>
+            <hr />
+          </article>
+        </div>
       )}
     </div>
   );
